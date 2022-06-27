dba3cf78171d1b166c38792a7aa1c8bc50c825eb
rexleonardo <rexpleonardo@gmail.com>
2022-06-27 10:19:36 +0800

    Login and Register Form

---
+++ components/Login.tsx
@@ -0,0 +1,9 @@
+import { MdMailOutline, MdLockOutline, MdOutlineVisibilityOff, MdOutlineVisibility, MdChevronRight } from 'react-icons/md'
+
+const Login: React.FC = () => {
+    return (
+        <></>
+    )
+}
+
+export default Login
---
+++ components/Register.tsx
@@ -0,0 +1,9 @@
+import { MdMailOutline, MdLockOutline, MdOutlineVisibilityOff, MdOutlineVisibility, MdChevronRight } from 'react-icons/md'
+
+const Register: React.FC = () => {
+    return (
+        <></>
+    )
+}
+
+export default Register
